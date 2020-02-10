7125daab18bdabcd405db6602afc254da4f1ad85
ezure haruna <burimofumofu@gmail.com>
2020-02-10 12:26:19 +0900

    ハンバーガーメニュー未完

--- main.js
+++ main.js
@@ -1,29 +1,23 @@
 //ハンバーガーメニューの出現
+
+
+
+function toggleNav() {
+  var body = document.body;
+  var ham = document.getElementById('js-ham');
+  var blackBg = document.getElementById('js-black-bg');
+
+  ham.addEventListener('click', function() {
+    body.classList.toggle('nav-open');
+  });
+  blackBg.addEventListener('click', function() {
+    body.classList.remove('nav-open');
+  });
+}
+toggleNav();
+
+
 /*
-const toggleMenu = document.getElementById('js-toggleMenu')
-//cf. const toggleBtn = document.querySelector('#js-toggleMenu')
-//ボタンを取得
-
-// 下記は配列ライクな形式で返ってくる
-const 
-// const menu  = document.getElementsByClassName('menu')[0]
-
-// ↓を使用すれば、クラスを指定しても最初の一つだけ取得できる
-const menu = document.querySelector('.menu')
-//動かす要素（出現するham)
-//querySelector→CSSでのセレクタ指定。cssと同じ書き方
-//querySelectorAllだと、複数指定できる("")
-
-toggleMenu.addEventListener('click', () => {
-    // activeがすでにmenuに付いていればactiveを削除
-    // ついていなければ追加
-    menu.classList.toggle('active')
-
-    menu.classList.remove("active")
-    menu.classList.add("active")
-    //表示のオンオフ
-})
-*/
 
 function toggleNav() {
     var body = document.body;
@@ -39,7 +33,7 @@ function toggleNav() {
   }
   toggleNav();
 
-
+*/
 
 
 
@@ -50,6 +44,9 @@ function toggleNav() {
 
 //スムーススクロール
 //クリックされた場所を特定する→（）の中にeを入れる
+
+
+
 document.addEventListener('click', (e) => {
     //e.targetでイベントの発生の発生元を特定できる！
     const　element =e.target
@@ -67,10 +64,14 @@ document.addEventListener('click', (e) => {
 
     //scrollIntoView関数を用いて特定の要素までスムーススクロールする
     //ジャンプ先の要素.scrollIntoView()
+
     jumpToSection.scrollIntoView({
         //スムーズにスクロールする
     　　behavior: "smooth",
     　　//先頭までスクロール
         block: "start"
     })
+    
 })
+
+
