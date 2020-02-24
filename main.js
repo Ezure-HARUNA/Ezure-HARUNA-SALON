5d2252f21f2c168d5ac5f28f5142876ccdc19bf2
ezure haruna <burimofumofu@gmail.com>
2020-02-24 20:04:09 +0900

    メニューの項目をクリックしたときにメニューを非表示にしたい

--- main.js
+++ main.js
@@ -1,50 +1,35 @@
 //ハンバーガーメニューの出現
 
-
-
 function toggleNav() {
+  //➀要素を取得する
+　//getElementById関数を使って要素を取得する
+　//引数にはidを文字列で指定する
   var body = document.body;
   var ham = document.getElementById('js-ham');
   var blackBg = document.getElementById('js-black-bg');
+  var menuBack =document.getElementById('js-back-menu');
 
+  //➁イベントリスナーを設定する
+  //対象の要素.addEventListener(イベント名, 処理)
   ham.addEventListener('click', function() {
     body.classList.toggle('nav-open');
   });
   blackBg.addEventListener('click', function() {
     body.classList.remove('nav-open');
   });
-}
+  document.addEventListener('click', () => {
+  if(element.classList.contains('scroll'))
+  body.classList.remove('nav-open');
+  //メニューをクリックした際、メニューを非表示にする
+  });
 toggleNav();
 
-
-/*
-
-function toggleNav() {
-    var body = document.body;
-    var hamburger = document.getElementById('js-ham');
-    var blackBg = document.getElementById('js-black-bg');
-  
-    hamburger.addEventListener('click', function() {
-      body.classList.toggle('nav-open');
-    });
-    blackBg.addEventListener('click', function() {
-      body.classList.remove('nav-open');
-    });
-  }
-  toggleNav();
-
-*/
-
-
-
-
-
+}
 
 
 
 //スムーススクロール
 //クリックされた場所を特定する→（）の中にeを入れる
-
 
 
 document.addEventListener('click', (e) => {
@@ -58,6 +43,10 @@ document.addEventListener('click', (e) => {
     //ジャンプ先の要素を取得する
     //docment.querySelector('#first')
     const jumpToSection = document.querySelector(element.hash)
+    
+  if (!element.classList.contains('scroll')
+  )
+  return ;
     //console.log(element, 'がクリックされた！')　
 
     //const targetSection =
@@ -66,6 +55,7 @@ document.addEventListener('click', (e) => {
     //ジャンプ先の要素.scrollIntoView()
 
     jumpToSection.scrollIntoView({
+        
         //スムーズにスクロールする
     　　behavior: "smooth",
     　　//先頭までスクロール
